82fcabfb14e5d7a9b84fe80974ab4e21eda0f08a
Vivek Nair <vnair611@gmail.com>
2023-04-25 06:43:00 -0400

    fix: wip

--- package/gentrace/providers/getters.py
+++ package/gentrace/providers/getters.py
@@ -1,6 +1,5 @@
 import os
 import re
-from urllib.parse import urlparse
 
 import openai
 
@@ -12,13 +11,22 @@ openai.api_key = os.getenv("OPENAI_KEY")
 VALID_GENTRACE_HOST = r"^https?://[\w.-]+:\d{1,5}/api/v1/?$"
 
 
+def test_validity():
+    from gentrace import api_key, host
+
+    if not api_key:
+        raise ValueError("Gentrace API key not set")
+
+    if host and not re.match(VALID_GENTRACE_HOST, host):
+        raise ValueError("Gentrace host is invalid")
+
+
 def configure_openai():
     from gentrace import api_key, host
 
     from .llms.openai import annotate_openai_module
 
-    if not api_key:
-        raise ValueError("Gentrace API key not set")
+    test_validity()
 
     if host and not re.match(VALID_GENTRACE_HOST, host):
         raise ValueError("Gentrace host is invalid")
@@ -30,12 +38,9 @@ def configure_openai():
 
 
 def configure_pinecone():
-    from gentrace import api_key, host
-
     from .vectorstores.pinecone import annotate_pinecone_module
 
-    if not api_key:
-        raise ValueError("Gentrace API key not set")
+    test_validity()
 
     annotate_pinecone_module()
 
