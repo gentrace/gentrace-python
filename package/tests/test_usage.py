ff42b8525282780244e171895f12ed3f5f811d50
Vivek Nair <vnair611@gmail.com>
2023-04-24 20:55:13 -0400

    fix: WIP

--- package/tests/test_usage.py
+++ package/tests/test_usage.py
@@ -1,10 +1,26 @@
 import os
+import re
 from unittest.mock import create_autospec
 
 import pytest
 from urllib3.response import HTTPResponse
 
 import gentrace
+
+
+def test_gentrace_host_valid():
+    gentrace.api_key = os.getenv("GENTRACE_API_KEY")
+    gentrace.host = "http://localhost:3000/"
+
+    with pytest.raises(ValueError):
+        gentrace.configure_openai()
+
+    gentrace.host = "http://localhost:3000/api/v1/feedback"
+    with pytest.raises(ValueError):
+        gentrace.configure_openai()
+
+    gentrace.host = ""
+    gentrace.api_key = ""
 
 
 def test_openai_configure_should_raise_error():
