4ab43b4c66385aa0c33f0ab32ad9219de673dee3
Vivek Nair <vnair611@gmail.com>
2023-04-27 07:49:51 -0400

    fix: wip

--- package/tests/test_openai_completion.py
+++ package/tests/test_openai_completion.py
@@ -397,3 +397,113 @@ def test_openai_completion_self_contained_no_pipeline_id_prompt(
     assert "pipelineRunId" not in result
 
     print(setup_teardown_openai)
+
+
+@responses.activate
+def test_openai_completion_self_contained_pipeline_id_stream_prompt(
+    mocker, completion_response, gentrace_pipeline_run_response, setup_teardown_openai
+):
+    openai.api_key = os.getenv("OPENAI_KEY")
+
+    # Setup OpenAI mocked request
+    openai_api_key_getter = mocker.patch.object(openai.util, "default_api_key")
+    openai_api_key_getter.return_value = "test-key"
+
+    responses.add(
+        responses.POST,
+        "https://api.openai.com/v1/completions",
+        body="data: " + json.dumps(completion_response, ensure_ascii=False),
+        stream=True,
+        content_type="text/event-stream",
+    )
+
+    # Setup Gentrace mocked response
+    headers = http.client.HTTPMessage()
+    headers.add_header("Content-Type", "application/json")
+
+    body = json.dumps(gentrace_pipeline_run_response, ensure_ascii=False).encode(
+        "utf-8"
+    )
+
+    gentrace_response = HTTPResponse(
+        body=body,
+        headers=headers,
+        status=200,
+        reason="OK",
+        preload_content=False,
+        decode_content=True,
+        enforce_content_length=True,
+    )
+
+    gentrace_request = mocker.patch.object(gentrace.api_client.ApiClient, "request")
+    gentrace_request.return_value = gentrace_response
+
+    result = openai.Completion.create(
+        pipeline_id="text-generation",
+        model="text-davinci-003",
+        prompt="Hello world!",
+        stream=True,
+    )
+
+    pipeline_run_id = None
+    for value in result:
+        pipeline_run_id = value["pipelineRunId"]
+
+    assert uuid.UUID(pipeline_run_id) is not None
+
+    print(setup_teardown_openai)
+
+
+@responses.activate
+def test_openai_completion_self_contained_no_pipeline_id_stream_prompt(
+    mocker, completion_response, gentrace_pipeline_run_response, setup_teardown_openai
+):
+    openai.api_key = os.getenv("OPENAI_KEY")
+
+    # Setup OpenAI mocked request
+    openai_api_key_getter = mocker.patch.object(openai.util, "default_api_key")
+    openai_api_key_getter.return_value = "test-key"
+
+    responses.add(
+        responses.POST,
+        "https://api.openai.com/v1/completions",
+        body="data: " + json.dumps(completion_response, ensure_ascii=False),
+        stream=True,
+        content_type="text/event-stream",
+    )
+
+    # Setup Gentrace mocked response
+    headers = http.client.HTTPMessage()
+    headers.add_header("Content-Type", "application/json")
+
+    body = json.dumps(gentrace_pipeline_run_response, ensure_ascii=False).encode(
+        "utf-8"
+    )
+
+    gentrace_response = HTTPResponse(
+        body=body,
+        headers=headers,
+        status=200,
+        reason="OK",
+        preload_content=False,
+        decode_content=True,
+        enforce_content_length=True,
+    )
+
+    gentrace_request = mocker.patch.object(gentrace.api_client.ApiClient, "request")
+    gentrace_request.return_value = gentrace_response
+
+    result = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Hello world!",
+        stream=True,
+    )
+
+    pipeline_run_id = None
+    for value in result:
+        if "pipelineRunId" in value:
+            pipeline_run_id = value["pipelineRunId"]
+
+    assert pipeline_run_id is None
+
+    print(setup_teardown_openai)
