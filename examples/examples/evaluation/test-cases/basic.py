673b45307be8d1f8e809b8b5eac99b8fd546d5e0
Vivek Nair <vivek@gentrace.ai>
2023-06-27 15:59:54 -0400

    feat: Accept output steps (#119)

--- examples/examples/evaluation/test-cases/basic.py
+++ examples/examples/evaluation/test-cases/basic.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SET_ID = "e605d843-88e0-4462-85cc-2d49b0217a30"
+SET_ID = "9685b34e-2cac-5bd2-8751-c9e34ff9fd98"
 
 gentrace.init(
-    api_key=os.getenv("GENTRACE_API_KEY"),
+    api_key=os.getenv("GENTRACE_API_KEY"), host="http://localhost:3000/api/v1"
 )
 
 cases = gentrace.get_test_cases(set_id=SET_ID)
--- examples/examples/evaluation/test-run/submit.py
+++ examples/examples/evaluation/test-run/submit.py
@@ -1,21 +1,31 @@
 import os
+from typing import List
 
 import gentrace
 from dotenv import load_dotenv
+from gentrace.providers.evaluation import OutputStep
 
 load_dotenv()
 
-SET_ID = "e605d843-88e0-4462-85cc-2d49b0217a30"
+SET_ID = "9685b34e-2cac-5bd2-8751-c9e34ff9fd98"
 
-gentrace.init(api_key=os.getenv("GENTRACE_API_KEY"), run_name="vivek python run")
+gentrace.init(
+    api_key=os.getenv("GENTRACE_API_KEY"),
+    run_name="vivek python run",
+    host="http://localhost:3000/api/v1",
+)
 
 cases = gentrace.get_test_cases(set_id=SET_ID)
 
-results = []
+outputs = []
+output_steps: List[List[OutputStep]] = []
 
 for case in cases:
-    results.append("This are some outputs")
+    outputs.append("This are some outputs")
+    output_steps.append([{"key": "compose", "output": "Testing information"}])
 
-result = gentrace.submit_test_results(SET_ID, test_cases=cases, outputs=results)
+result = gentrace.submit_test_results(
+    SET_ID, test_cases=cases, outputs=outputs, output_steps=output_steps
+)
 
 print(result["runId"])
