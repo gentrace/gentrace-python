f7a08c303f7271e9d75bdfe277989112e3ea0539
Vivek Nair <vnair611@gmail.com>
2023-09-21 07:41:45 -0400

    fix: wip

--- examples/examples/evaluation/test-result/submit-old.py
+++ examples/examples/evaluation/test-result/submit-old.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SET_ID = "c10408c7-abde-5c19-b339-e8b1087c9b64"
-
 gentrace.init(
     api_key=os.getenv("GENTRACE_API_KEY"),
     run_name="vivek python run",
@@ -29,4 +27,4 @@ result = gentrace.submit_test_result(
     "main", test_cases=cases, outputs_list=outputs_list
 )
 
-print(result["runId"])
+print(result["resultId"])
