e2717cf09e2e52dcd2cdd31cc335bd2c66f9deef
Vivek Nair <vnair611@gmail.com>
2024-07-01 18:05:31 -0400

    fix: wip

---
+++ examples/examples/simple/core/testing-ameasure.py
@@ -0,0 +1,60 @@
+import asyncio
+import os
+from multiprocessing.pool import ThreadPool
+
+import gentrace
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def example_response(inputs):
+    return "This is a generated response from the AI"
+
+
+def enable_parallelism(function, inputs, parallel_threads):
+    # modify this parallelization approach as needed
+    pool = ThreadPool(processes=parallel_threads)
+    outputs = pool.map(function, inputs)
+    return outputs
+
+
+gentrace.init(
+    api_key=os.getenv("GENTRACE_API_KEY"),
+    host="http://localhost:3000/api",
+)
+
+# example existing pipelines
+
+PIPELINE_SLUG = "guess-the-year"
+pipeline_by_slug = gentrace.Pipeline(PIPELINE_SLUG)
+
+PIPELINE_ID = "c10408c7-abde-5c19-b339-e8b1087c9b64"
+pipeline_by_id = gentrace.Pipeline(id=PIPELINE_ID)
+
+pipeline = pipeline_by_slug
+
+
+async def measure_func(inputs):
+    return example_response(inputs)
+
+
+async def example_handler(pipeline_run_test_case):
+    (runner, test_case) = pipeline_run_test_case
+    await runner.ameasure(
+        measure_func,
+        inputs=test_case.get("inputs")
+    )
+
+
+async def main():
+    pipeline_run_test_cases = gentrace.get_test_runners(pipeline)
+
+    for pipeline_run_test_case in pipeline_run_test_cases:
+        await example_handler(pipeline_run_test_case)
+
+    result = gentrace.submit_test_runners(pipeline, pipeline_run_test_cases)
+    print("Result: ", result)
+
+
+asyncio.run(main())
