4fa0928bfee4b32a3bc55e5b0bb60f22a8e3ddf6
meorphis <eric@stainless.com>
2025-07-17 21:12:53 -0400

    chore: fix version

--- src/gentrace/_version.py
+++ src/gentrace/_version.py
@@ -1,4 +1,4 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 __title__ = "gentrace"
-__version__ = "1.0.0-alpha.5"  # x-release-please-version
+__version__ = "1.0.0-alpha.13"  # x-release-please-version
