36ce6aab16fd45c38f9e3e3db457fae89d6118bc
Vivek Nair <vivek@gentrace.ai>
2025-07-24 12:46:25 -0400

    fix: align conftest pytest marker with session scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,7 +26,7 @@ logging.getLogger("gentrace").setLevel(logging.DEBUG)
 # so we don't have to add that boilerplate everywhere
 def pytest_collection_modifyitems(items: list[pytest.Function]) -> None:
     pytest_asyncio_tests = (item for item in items if is_async_test(item))
-    session_scope_marker = pytest.mark.asyncio(loop_scope="function")
+    session_scope_marker = pytest.mark.asyncio(loop_scope="session")
     for async_test in pytest_asyncio_tests:
         async_test.add_marker(session_scope_marker, append=False)
 
